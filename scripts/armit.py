62bab3a9da817626b7d157abaf621e0d145f544f
Oren Gordon <orengor@post.bgu.ac.il>
2016-05-10 20:38:17 +0300

    Finished movement and arm

--- scripts/armit.py
+++ scripts/armit.py
@@ -5,7 +5,7 @@ from robot_miniproj.msg import *
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 
-shoulder= None
+shoulder = None
 elevator = None
 base_rotation = None
 wrist = None
@@ -15,53 +15,62 @@ elbow = None
 
 def armit(data):
     print "Arm is moving"
-    rospy.sleep(int(abs(data.radians) * 20))
-    rospy.sleep(int(abs(data.distance) * 15))
-    global shoulder,elevator,base_rotation,wrist,left_finger,right_finger,elbow
+    global shoulder, elevator, base_rotation, wrist, left_finger, right_finger, elbow
 
-    print 'Moving arm to initial position'
-    for i in range(150):
-        elevator.publish(1)
-        shoulder.publish(1.7)
-        base_rotation.publish(0)
-        wrist.publish(0)
-        left_finger.publish(-1)
-        right_finger.publish(1)
-        elbow.publish(0.4)
-        rospy.sleep(0.1)
+    print 'Raising elevator'
+    # for i in range(200):
+    elevator.publish(0.35)
+    shoulder.publish(1.7)
+    base_rotation.publish(0)
+    wrist.publish(0)
+    left_finger.publish(0.33)
+    right_finger.publish(-0.33)
+    elbow.publish(0.4)
+    # rospy.sleep(0.1)
 
-    print 'Lowering elevator to grip position'
-    for i in range(200):
-        elevator.publish(0)
-        shoulder.publish(1.7)
-        base_rotation.publish(0)
-        wrist.publish(0)
-        left_finger.publish(-1)
-        right_finger.publish(1)
-        elbow.publish(0.4)
-        rospy.sleep(0.1)
+    # print 'Moving arm to initial position'
+    # for i in range(150):
+    #     elevator.publish(1)
+    #     shoulder.publish(1.7)
+    #     base_rotation.publish(0)
+    #     wrist.publish(0)
+    #     left_finger.publish(-1)
+    #     right_finger.publish(1)
+    #     elbow.publish(0.4)
+    #     rospy.sleep(0.1)
+
+    # print 'Lowering elevator to grip position'
+    # for i in range(200):
+    #     elevator.publish(0)
+    #     shoulder.publish(1.7)
+    #     base_rotation.publish(0)
+    #     wrist.publish(0)
+    #     left_finger.publish(-1)
+    #     right_finger.publish(1)
+    #     elbow.publish(0.4)
+    #     rospy.sleep(0.1)
      
-    print 'Gripping object'
-    for i in range(50):
-        elevator.publish(0)
-        shoulder.publish(1.7)
-        base_rotation.publish(0)
-        wrist.publish(0)
-        left_finger.publish(0.33)
-        right_finger.publish(-0.33)
-        elbow.publish(0.4)
-        rospy.sleep(0.1)
+    # print 'Gripping object'
+    # for i in range(50):
+    #     elevator.publish(0)
+    #     shoulder.publish(1.7)
+    #     base_rotation.publish(0)
+    #     wrist.publish(0)
+    #     left_finger.publish(0.33)
+    #     right_finger.publish(-0.33)
+    #     elbow.publish(0.4)
+    #     rospy.sleep(0.1)
 
-    print 'Moving to finish position'
-    for i in range(400):
-        elevator.publish(1)
-        shoulder.publish(1.7)
-        base_rotation.publish(0)
-        wrist.publish(0)
-        left_finger.publish(0.33)
-        right_finger.publish(-0.33)
-        elbow.publish(0.4)
-        rospy.sleep(0.1)
+    # print 'Moving to finish position'
+    # for i in range(400):
+    #     elevator.publish(1)
+    #     shoulder.publish(1.7)
+    #     base_rotation.publish(0)
+    #     wrist.publish(0)
+    #     left_finger.publish(0.33)
+    #     right_finger.publish(-0.33)
+    #     elbow.publish(0.4)
+    #     rospy.sleep(0.1)
 
 if __name__ == '__main__':
     try:
@@ -73,18 +82,11 @@ if __name__ == '__main__':
         left_finger = rospy.Publisher('/komodo_1/left_finger_controller/command', Float64, queue_size = 10)
         right_finger = rospy.Publisher('/komodo_1/right_finger_controller/command', Float64, queue_size = 10)
         elbow = rospy.Publisher('/komodo_1/elbow2_controller/command', Float64, queue_size = 10)
-        # print 'Raising elevator'
-        for i in range(300):
-            elevator.publish(1)
-            shoulder.publish(0)
-            base_rotation.publish(0)
-            wrist.publish(0)
-            left_finger.publish(-1)
-            right_finger.publish(1)
-            elbow.publish(0)
-            rospy.sleep(0.1)
+        
+        rospy.sleep(20)
+        elevator.publish(1)
         print "Arm is now active"
-        rospy.Subscriber('advance', Distance, armit)
+        rospy.Subscriber('arm', Distance, armit)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
